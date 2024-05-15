aaa122d8b38501acbc33c92523bc2e9785014f86
arnab <arn123go@gmail.com>
2024-05-15 22:08:06 +0600

    made oop.php prettier

--- oop.php
+++ oop.php
@@ -5,16 +5,16 @@ class Library {
     private $books;
     private $members;
 
-    public function addBook(Book $book){
+    public function addBook( Book $book ) {
         $this->books[] = $book;
     }
-    
-    public function addMember(Member $member){
+
+    public function addMember( Member $member ) {
         $this->members[] = $member;
     }
 
-    public function displayAvailableBooks(){
-        foreach ($this->books as $book){
+    public function displayAvailableBooks() {
+        foreach ( $this->books as $book ) {
             echo "Available Copies of '" . $book->getTitle() . "': " . $book->getAvailableCopies() . "\n";
         }
     }
@@ -24,24 +24,24 @@ class Book {
     private $title;
     private $availableCopies;
 
-    public function __construct($title, $availableCopies){
+    public function __construct( $title, $availableCopies ) {
         $this->title = $title;
         $this->availableCopies = $availableCopies;
     }
 
-    public function getTitle(){
+    public function getTitle() {
         return $this->title;
     }
 
-    public function getAvailableCopies(){
+    public function getAvailableCopies() {
         return $this->availableCopies;
     }
 
-        public function borrowBook(){
+    public function borrowBook() {
         $this->availableCopies -= 1;
     }
 
-    public function returnBook(){
+    public function returnBook() {
         $this->availableCopies += 1;
     }
 }
@@ -49,39 +49,39 @@ class Book {
 class Member {
     private $name;
 
-    public function __construct($name){
+    public function __construct( $name ) {
         $this->name = $name;
     }
-    
-    public function getName(){
+
+    public function getName() {
         return $this->name;
     }
-    
-    public function borrowBook(Book $book){
+
+    public function borrowBook( Book $book ) {
         $book->borrowBook();
     }
-    public function returnBook(Book $book){
+    public function returnBook( Book $book ) {
         $book->returnBook();
     }
 }
 
-$book1 = new Book("The Great Gatsby", 5);
-$book2 = new Book("To Kill a Mockingbird", 3);
+$book1 = new Book( "The Great Gatsby", 5 );
+$book2 = new Book( "To Kill a Mockingbird", 3 );
 
-$member1 = new Member("John Doe");
-$member2 = new Member("Jane Smith");
+$member1 = new Member( "John Doe" );
+$member2 = new Member( "Jane Smith" );
 
 $library = new Library;
 
-$library->addBook($book1);
-$library->addBook($book2);
+$library->addBook( $book1 );
+$library->addBook( $book2 );
 
-$library->addMember($member1);
-$library->addMember($member2);
+$library->addMember( $member1 );
+$library->addMember( $member2 );
 
-$member1->borrowBook($book1);
+$member1->borrowBook( $book1 );
 // $member1->returnBook($book1);
-$member2->borrowBook($book2);
+$member2->borrowBook( $book2 );
 // $member2->returnBook($book2);
 
 $library->displayAvailableBooks();
